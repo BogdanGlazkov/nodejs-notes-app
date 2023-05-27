d2b18dac961d8e18683632d750bec7d1a44aaa14
BogdanGlazkov <bglazkov@i.ua>
2023-05-27 23:19:30 +0300

    Add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,16 @@
-import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import app from "./app";
 import env from "./util/validateEnv";
 
-const app = express();
 const PORT = env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
 
 mongoose
   .connect(env.MONGO_DB_URL)
   .then(() => {
     console.log("Mongoose connected");
     app.listen(PORT, () => {
-      console.log("Server running on port " + PORT);
+      console.log("Server running on port " + env.PORT);
     });
   })
   .catch(console.error);
