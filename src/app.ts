e55a24293ec69b3352fc93b095bd475d27cbdab8
BogdanGlazkov <bglazkov@i.ua>
2023-06-01 00:00:36 +0300

    Add user session

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,33 @@
 import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import notesRoutes from "./routes/notes";
+import usersRoutes from "./routes/users";
+import env from "./utils/validateEnv";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_DB_URL,
+    }),
+  })
+);
 
+app.use("/api/users", usersRoutes);
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
